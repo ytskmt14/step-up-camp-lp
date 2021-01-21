a9b06ce7bcbceab7d62499d6b956686870d74f5f
yuta sakamoto <sorigeeeen@gmail.com>
2021-01-21 22:18:16 +0900

    modify about section

--- src/components/section/About.vue
+++ src/components/section/About.vue
@@ -1,5 +1,72 @@
 <template>
   <div id="about" class="section">
-    <h1>About</h1>
+    <v-container>
+      <v-row
+        justify="center"
+        align="start"
+      >
+        <v-col
+          align="start"
+          md="4"
+        >
+          <div class="text-center text-h4 font-weight-bold">
+            ABOUT
+          </div>
+          <div class="text-center text-h7 ">
+            ステキャンについて
+          </div>
+        </v-col>
+      </v-row>
+      <v-row
+        justify="center"
+        align="start"
+      >
+        <v-col
+          align="start"
+          md="8"
+        >
+          <div class="mt-5 text-left text-body-2">
+            <p>ステキャンとは、ステップアップキャンプの略称です。</p>
+            <p>それぞれが興味のあることや勉強したことなどをみんなに共有していく場として、2019年10月からスタートしました。</p>
+            <p>
+              発表のテーマは、業務に関係あることでもないことでもなんでもOK！
+              過去にはこんなテーマがありました。
+            </p>
+            <ul class="about-list">
+              <li
+                v-for="(pastTheme, index) in pastThemeList"
+                :key="index"
+              >
+                {{ pastTheme }}
+              </li>
+            </ul>
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      pastThemeList: [
+        "情報セキュリティきほんの「き」",
+        "RUBY ON RAILS!!",
+        "生産性の向上ってどういうこと？",
+        "Slack活用術",
+        "Django入門",
+        "etc...",
+        ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+.about-list {
+  list-style: none;
+  text-indent: -1.5em;
+}
+</style>
