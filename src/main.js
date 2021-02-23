3dcdb0612cf4a47d5c0ba083f52e5d38e86bc7e2
yuta sakamoto <sorigeeeen@gmail.com>
2021-02-23 22:50:00 +0900

    add vue-head lib

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
+import VueHead from 'vue-head'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import vueSmoothScroll from 'vue-smooth-scroll'
 
 Vue.config.productionTip = false
 Vue.use(vueSmoothScroll)
+Vue.use(VueHead)
 
 new Vue({
   vuetify,
