c7c92538b35a4a6ff0d0df084ec37b1862a69144
健吾 前田 <ke-maeda@seraku.co.jp>
2021-03-10 23:31:49 +0900

    ga4 対応

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app>
-      <Header></Header>
+    <Header></Header>
     <v-main>
       <v-container fluid>
         <Top></Top>
@@ -14,16 +14,23 @@
 </template>
 
 <script>
-const Header = () => import(/* webpackChunkName: "Header" */ './components/section/Header.vue');
-const Top = () => import(/* webpackChunkName: "Top" */ './components/section/Top.vue');
-const About = () => import(/* webpackChunkName: "About" */ './components/section/About.vue');
-const Works = () => import(/* webpackChunkName: "Works" */ './components/section/Works.vue');
-const JoinUs = () => import(/* webpackChunkName: "JoinUs" */ './components/section/JoinUs.vue');
-const Features = () => import(/* webpackChunkName: "Features" */'./components/section/Features.vue');
+const Header = () =>
+  import(/* webpackChunkName: "Header" */ "./components/section/Header.vue");
+const Top = () =>
+  import(/* webpackChunkName: "Top" */ "./components/section/Top.vue");
+const About = () =>
+  import(/* webpackChunkName: "About" */ "./components/section/About.vue");
+const Works = () =>
+  import(/* webpackChunkName: "Works" */ "./components/section/Works.vue");
+const JoinUs = () =>
+  import(/* webpackChunkName: "JoinUs" */ "./components/section/JoinUs.vue");
+const Features = () =>
+  import(
+    /* webpackChunkName: "Features" */ "./components/section/Features.vue"
+  );
 
 export default {
-  name: 'App',
-
+  name: "App",
   components: {
     Header,
     Top,
@@ -34,9 +41,21 @@ export default {
   },
   head: {
     title: {
-      inner: 'Welcome',
-      complement: 'step up camp'
-    }
+      inner: "Welcome",
+      complement: "step up camp",
+    },
+    script: [
+      {
+        type: "text/javascript",
+        src: "src=https://www.googletagmanager.com/gtag/js?id=G-SWYJ0FYSTR",
+        async: true,
+      },
+      {
+        type: "text/javascript",
+        inner:
+          "window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-SWYJ0FYSTR');",
+      },
+    ],
   },
 };
 </script>
@@ -45,5 +64,4 @@ export default {
 .body {
   background-color: #efefef;
 }
-
 </style>
